38be02688408875528592e81ec9585143ec8b59a
S_HE <stanhe@gmail.com>
2019-11-15 19:08:14 -0800

    some ui stuff

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,8 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-        <h1>Warda</h1>
+        <h1 style = {{position: 'absolute', left: '50%', top: '10%',
+            transform: 'translate(-50%, -50%)'}}>Worda</h1>
           <FileButton />     
       </div>
     );
--- client/src/components/FileButton.js
+++ client/src/components/FileButton.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import ReactFileReader from 'react-file-reader';
 import Recorder from 'react-mp3-recorder'
-import { saveAs } from 'file-saver';
 import FileSaver from 'file-saver'; 
+import { blobToBase64String } from 'blob-util'
 
 export default class FileButton extends Component {
 
     _onRecordingComplete = (blob) => {
         console.log('recording', blob)
+        blob.name = 'audio.mp3'
         FileSaver.saveAs(blob, "audio.mp3")
       }
      
@@ -18,14 +19,18 @@ export default class FileButton extends Component {
     handleFiles = files => {
         console.log(files)
     }
+
+    getStyle = () => {
+        return {
+            position: 'absolute', left: '50%', top: '25%',
+            transform: 'translate(-50%, -50%)'
+        }
+    }
     
     render() {
         return (
-            <div>
-                <ReactFileReader handleFiles={this.handleFiles}>
-                    <button className='btn'>Record</button>
-                </ReactFileReader>
-                <Recorder
+            <div style={this.getStyle()}>
+                <Recorder 
                     onRecordingComplete={this._onRecordingComplete}
                     onRecordingError={this._onRecordingError}
                 />
